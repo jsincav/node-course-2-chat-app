9a3e504d436f0dfbe77fdfdcc5287eab7d2e74a0
Jeff Sincavage <jsincav44@gmail.com>
2018-01-09 21:21:14 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to sever');
 
-    socket.emit('createMessage', {
-        to: 'jen@example.com',
-        text: 'Hey. This is Andrew'
-    });
 });
 
 socket.on('disconnect', function () {
